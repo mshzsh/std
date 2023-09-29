541a6a09e4b8f283a4bc6d763ee5c370ceae7e3e
Mshzsh <mshzsh@gmail.com>
2023-09-28 19:57:27 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,101 @@
+#include <gtk/gtk.h>
+#include <gdk/gdkwayland.h>
+#include <wayland-client.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// Wayland globals
+static struct wl_display *display = NULL;
+static struct wl_registry *registry = NULL;
+static struct wl_compositor *compositor = NULL;
+static struct wl_seat *seat = NULL;
+static struct wl_surface *surface = NULL;
+
+// GTK globals
+static GtkWidget *panel;
+static GtkWidget *textbox;
+
+static void registry_handle_global(
+    void *data,
+    struct wl_registry *registry,
+    uint32_t id,
+    const char *interface,
+    uint32_t version
+) {
+    // Handle Wayland global objects here, e.g., wl_compositor, wl_seat
+}
+
+static void registry_handle_global_remove(
+    void *data,
+    struct wl_registry *registry,
+    uint32_t name
+) {
+    // Handle removal of Wayland global objects here
+}
+
+static const struct wl_registry_listener registry_listener = {
+    registry_handle_global,
+    registry_handle_global_remove
+};
+
+static void on_textbox_activate(GtkEntry *entry, gpointer user_data) {
+    // Handle textbox activation (e.g., close the app)
+    const gchar *text = gtk_entry_get_text(entry);
+    g_print("Text entered: %s\n", text);
+
+    // Use the "wl-clipboard" command-line tool to copy text to clipboard
+    char *command = g_strdup_printf("wl-copy \"%s\"", text);
+    system(command);
+    g_free(command);
+
+    gtk_main_quit();
+}
+
+
+static void setup_wayland() {
+    display = wl_display_connect(NULL);
+    registry = wl_display_get_registry(display);
+    wl_registry_add_listener(registry, &registry_listener, NULL);
+    wl_display_roundtrip(display);
+}
+
+int main(int argc, char **argv) {
+    gtk_init(&argc, &argv);
+
+    // Initialize Wayland
+    setup_wayland();
+
+    // Create a GTK window (panel)
+    panel = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(panel), "STD");
+    gtk_window_set_default_size(GTK_WINDOW(panel), 800, 50);
+    g_signal_connect(panel, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    // Create a GTK entry (textbox)
+    textbox = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(textbox), "Type here...");
+    gtk_widget_set_name(textbox, "textbox"); // Set a custom name for the textbox
+
+    // Load and apply the CSS file to the entire application
+    GtkCssProvider *cssProvider = gtk_css_provider_new();
+    gtk_css_provider_load_from_path(cssProvider, "styles.css", NULL);
+    gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    g_signal_connect(textbox, "activate", G_CALLBACK(on_textbox_activate), NULL);
+
+    // Add the textbox to the panel
+    gtk_container_add(GTK_CONTAINER(panel), textbox);
+
+    // Show everything
+    gtk_widget_show_all(panel);
+
+    // Enter the GTK main loop
+    gtk_main();
+
+    // Cleanup Wayland
+    wl_surface_destroy(surface);
+    wl_seat_destroy(seat);
+    wl_registry_destroy(registry);
+    wl_display_disconnect(display);
+
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Secure Text Display Application for Wayland Linux
+
+## Overview
+
+The Secure Text Display Application for Wayland Linux is designed to address several critical needs in specific workspaces and scenarios:
+
+1. **Quick Text Manipulation:** The application allows you to efficiently copy and manipulate text, making it easier to change and reuse sentences quickly.
+
+2. **Password Input Verification:** Another crucial use case is entering passwords securely. By writing and copying a password before entering it into a website's input field, you can verify its accuracy even in the presence of potential keylogger malware on the target site.
+
+3. **Wayland Linux Compatibility:** This project is specifically designed for Wayland Linux environments.
+
+## Prerequisites
+
+Before using the Secure Text Display Application, ensure you have the following prerequisites in place:
+
+- A Wayland Linux environment.
+- GTK+ and Wayland libraries.
+- The "wl-clipboard" tool (for copying text to the clipboard).
+
+## Usage
+
+Using the application is straightforward:
+
+1. **Compile and Run:** Compile and run the application in your Wayland Linux environment.
+
+2. **Enter Text:** Enter the text or password you want to work with into the provided textbox.
+
+3. **Copy Text:** Press the "Enter" key to copy the text to the clipboard securely. This action also closes the application.
+
+4. **Paste and Use:** You can now paste the copied text into other applications or websites using "wl-paste."
+
+## Custom Styling
+
+The application allows you to customize the appearance of the text box. Edit the included `styles.css` file to adjust properties like background color, text color, and font size to suit your preferences.
+
+## Contributing
+
+Contributions, suggestions, and improvements to this open-source project are highly welcomed. To contribute, fork the repository on GitHub and submit pull requests.
+
+---
+
+Feel free to modify and adapt this README.md content to fit your project's specifics. Once you've created the `.md` file, you can upload it to your GitHub repository following the same steps I mentioned earlier.
+
+If you have any further questions or need additional assistance, please let me know!
